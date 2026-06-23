8048902733d97ed25e42dbb2b5b64629f25deb47
anandagireesh91 <anand@powernsun.com>
2026-06-23 14:11:50 +0400

    updated sidebar

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -131,16 +131,6 @@
             </a>
         </div>
 
-        {{-- Offers --}}
-        <a href="{{ route('admin.offers.index') }}"
-           class="nav-item {{ request()->routeIs('admin.offers.*') ? 'active' : '' }}">
-            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"/>
-                <line x1="7" y1="7" x2="7.01" y2="7"/>
-            </svg>
-            Offers
-        </a>
-
         {{-- Warehouses --}}
         <a href="{{ route('admin.warehouses.index') }}"
            class="nav-item {{ request()->routeIs('admin.warehouses.*') ? 'active' : '' }}">
